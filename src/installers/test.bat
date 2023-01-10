50e9777d16eb5f3e32d90f97b80fc5a4d61578b6
KRW <classic.krw@gmail.com>
2023-01-10 14:28:24 +0100

    Organisation

--- src/installers/test.bat
+++ src/installers/test.bat
@@ -7,7 +7,7 @@ cd test
 cls
 
 rem Download test file from github with diffrent name
-curl https://raw.githubusercontent.com/KRWCLASSIC/OptiPack/master/src/misc/download-sources/test_download.txt -o test_downloadED.txt
+curl https://raw.githubusercontent.com/KRWCLASSIC/OptiPack/master/src/misc/test_download.txt -o test_downloadED.txt
 echo.
 
 rem Going back to the installer.bat and closing this script
