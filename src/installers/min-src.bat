985688771c501966da2465f88d63bdc720a45eca
KRW <classic.krw@gmail.com>
2023-01-11 15:41:48 +0100

    Added links to full-src.txt and added bad try of downloading all files from one url in min-src.bat

--- src/installers/min-src.bat
+++ src/installers/min-src.bat
@@ -1,1 +1,21 @@
+@echo on
+cd ..
+echo %cd%
+
+set "where=%CD%\misc\download-sources"
+set "filename=full-src.txt"
+set "dlpath=%where%\%filename%"
+set "savefolder=%CD%\temp"
+
+for /F "delims=" %%i in (%dlpath%) do (
+    set "url=%%i"
+    curl -r 0- -np -s -l -o "%savefolder%\%%~nxi" "%url%"
+)
+
+echo bruuuuuuuuuuuh
+echo some serious skill issue right there
+echo why isnt this working
+echo im gonna cry rn
+echo yk what? im gonna fix it later lmao
+pause
 exit
