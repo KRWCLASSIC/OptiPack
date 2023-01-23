f6616f4b6211e36d9a3ac1253782596a4c66b6de
KRW <classic.krw@gmail.com>
2023-01-23 02:48:12 +0100

    imported mods security from my other project

--- src/misc/post-init.bat
+++ src/misc/post-init.bat
@@ -4,6 +4,21 @@ rem This file is executed when any of the *-src.bat files finishes its work, thi
 rem Load minecraft path to a post-init.bat to finish installing procedure
 set /p mc-dir=<mc-dir.txt
 
+rem Setting %num% to 0
+set "num=0"
+
+rem Saving downloaded mods from combining with mods in ".minecraft/mods" procedure, basicly checks if there is any *.jar file in mods dir, if there is archive it in a folder
+rem loop that adds 1 to the %num% variable if there is Archive folder with that number
+:checkloop
+set /a num+=1
+if exist "%USERPROFILE%\%mc-dir%\mods\Archive%num%" goto checkloop
+
+rem Create the new Archive folder
+mkdir "%USERPROFILE%\%mc-dir%\mods\Archive%num%"
+rem Move all *.jar files to the Archive folder with current value of %num% variable ()
+move "%USERPROFILE%\%mc-dir%\mods\*.jar" "%USERPROFILE%\%mc-dir%\mods\Archive%num%"
+
+
 rem Moving mods procedure
 cd ..
 cd temp
